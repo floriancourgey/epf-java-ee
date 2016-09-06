c720bbe8399631d995f2e4b8846bcc0c32b9949e
Florian Courgey <florian@floriancourgey.com>
2016-09-06 09:59:29 +0200

    + models Company & Computer

---
+++ cours 1/src/main/java/com/floriancourgey/java/cours1/Cours1.java
@@ -0,0 +1,7 @@
+package com.floriancourgey.java.cours1;
+
+public class Cours1 {
+	public Cours1(){
+		System.out.println("yolo");
+	}
+}
---
+++ cours 1/src/main/java/com/floriancourgey/java/cours1/models/Computer.java
@@ -0,0 +1,46 @@
+package com.floriancourgey.java.cours1.models;
+
+import java.util.Date;
+
+public class Computer {
+	private long id;
+	private String name;
+	private Date introduced;
+	private Date discontinued;
+	private Company company;
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(Date introduced) {
+		this.introduced = introduced;
+	}
+	public Date getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(Date discontinued) {
+		this.discontinued = discontinued;
+	}
+	public Company getCompany() {
+		return company;
+	}
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+	
+	
+}
